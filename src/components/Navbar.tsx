773e67e02346062a3d3cb70e1624ba114ccf54d8
sima <simaruhussen@gmail.com>
2024-07-31 09:09:55 -0700

    build Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import { HStack, Image } from '@chakra-ui/react'
+import React from 'react'
+import logo from "../assets/logo.webp"
+
+const Navbar = () => {
+  return (
+    <HStack>
+        <Image src={logo} boxSize="60px"/>
+    </HStack>
+       
+  )
+}
+
+export default Navbar
