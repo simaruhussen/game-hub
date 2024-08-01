e4e77f21bb2849a5c9eeb476ef2f46bcb55a680b
sima <simaruhussen@gmail.com>
2024-08-01 08:00:15 -0700

    create acustem hook to fetch agame

--- src/services/api-clients.ts
+++ src/services/api-clients.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export default axios.create({
-    baseURL:"https://api.rawg.io/api/games",
+    baseURL:"https://api.rawg.io/api",
     params:{
         key:"06c0a17a04f34619893935bd50044928"
     }
